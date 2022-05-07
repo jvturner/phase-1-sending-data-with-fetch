ca9453796447d8d50baebd631bc0c7f206934eaf
Jamond Turner <jamondturner@yahoo.com>
2022-05-07 01:44:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: "Hulk Hogan",
+        email: "jim@aol.com",
+    }),
+};
+
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+        })
+.catch(function (error){
+    alert("DANGER!, JOHNNY 5, Gotta Have A METHOD YOUNG JEDI");
+    console.log(error.message);
+})
+};
+
+// NOTES 
